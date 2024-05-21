abaa911a23015d2e47cfe312ca77a5fabee5b2d8
Alec Pizziferro <alecpizz@gmail.com>
2024-05-20 21:11:56 -0500

    functional minimap lil guy

--- Assets/Scripts/UI/TabbedMenu.cs
+++ Assets/Scripts/UI/TabbedMenu.cs
@@ -15,19 +15,64 @@ namespace PlaceboEntertainment.UI
         private const string ContentNameSuffix = "Content";
         private const string HideClassName = "unselectedContent";
         private const string PlayerName = "Player";
+        [SerializeField] private Transform playerTransform;
+
+        [SerializeField] private Transform mapBoundaryUpperLeft,
+            mapBoundaryUpperRight,
+            mapBoundaryLowerLeft,
+            mapBoundaryLowerRight;
+
+        private void OnDrawGizmos()
+        {
+            if (!mapBoundaryLowerLeft || !mapBoundaryLowerRight || !mapBoundaryUpperLeft ||
+                !mapBoundaryUpperRight) return;
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawLine(mapBoundaryUpperLeft.position, mapBoundaryUpperRight.position);
+            Gizmos.DrawLine(mapBoundaryUpperRight.position, mapBoundaryLowerRight.position);
+            Gizmos.DrawLine(mapBoundaryLowerRight.position, mapBoundaryLowerLeft.position);
+            Gizmos.DrawLine(mapBoundaryLowerLeft.position, mapBoundaryUpperLeft.position);
+        }
+
+        private void Awake()
+        {
+            var menu = GetComponent<UIDocument>();
+            _root = menu.rootVisualElement;
+            _playerObject = _root.Q(PlayerName);
+        }
 
         private void OnEnable()
         {
-            UIDocument menu = GetComponent<UIDocument>();
-            _root = menu.rootVisualElement;
-            _playerObject = _root.Q(PlayerName);
             RegisterTabCallbacks();
+        }
+
+        private void OnDisable()
+        {
+            UnRegisterTabCallbacks();
+        }
+
+        private void Update()
+        {
+            if (!mapBoundaryLowerLeft || !mapBoundaryLowerRight || !mapBoundaryUpperLeft ||
+                !mapBoundaryUpperRight || _playerObject == null) return;
+            float xAxis = Mathf.InverseLerp(mapBoundaryUpperLeft.position.x,
+                mapBoundaryUpperRight.position.x,playerTransform.position.x);
+            float yAxis = Mathf.InverseLerp(mapBoundaryUpperLeft.position.z, mapBoundaryLowerLeft.position.z,
+                playerTransform.position.z);
+            var parent = _playerObject.parent;
+            _playerObject.style.translate =
+                new Translate((xAxis * parent.resolvedStyle.width), (yAxis * parent.resolvedStyle.height));
         }
 
         private void RegisterTabCallbacks()
         {
             UQueryBuilder<Label> tabs = GetAllTabs();
             tabs.ForEach(tab => { tab.RegisterCallback<ClickEvent>(TabOnClick); });
+        }
+        
+        private void UnRegisterTabCallbacks()
+        {
+            UQueryBuilder<Label> tabs = GetAllTabs();
+            tabs.ForEach(tab => { tab.UnregisterCallback<ClickEvent>(TabOnClick); });
         }
 
         private void TabOnClick(ClickEvent evt)
