cd224c9ea3cbaef89225744d209a92372f977274
NGrinstead0101 <ngrinstead@mail.bradley.edu>
2024-05-23 15:00:14 -0500

    Initial Implementation

---
+++ Assets/Scripts/NpcBehaviors/RobotNpc.cs
@@ -0,0 +1,88 @@
+/******************************************************************
+*    Author: Nick Grinstead
+*    Contributors: 
+*    Date Created: 5/22/24
+*    Description: NPC class containing logic for the Robot NPC.
+*******************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotNpc : BaseNpc
+{
+    [SerializeField] private NpcEvent unlockDoorsEvent;
+
+    [SerializeField] private float _timeToDeath;
+    private float _timeElapsed = 0f;
+
+    private bool _hasLightbulb = false;
+
+    /// <summary>
+    /// Called when engaging in dialogue during idle state and when recieving
+    /// an event upon minigame completion
+    /// </summary>
+    public override void CheckForStateChange()
+    {
+        if (_hasLightbulb && _currentState == NpcStates.DefaultIdle)
+        {
+            StopAllCoroutines();
+
+            EnterMinigameReady();
+        }
+
+        if (_currentState == NpcStates.PlayingMinigame)
+        {
+            EnterPostMinigame();
+        }
+    }
+
+    /// <summary>
+    /// Starts death timer
+    /// </summary>
+    protected override void EnterIdle()
+    {
+        base.EnterIdle();
+
+        StartCoroutine("DeathTimer");
+    }
+
+    /// <summary>
+    /// Moves straight into pre-minigame dialogue with bypass check
+    /// </summary>
+    protected override void EnterMinigameReady()
+    {
+        base.EnterMinigameReady();
+
+        Interact();
+        // TODO: add bypass check with minigame trigger?
+    }
+
+    /// <summary>
+    /// Moves straight into post-minigame dialogue
+    /// </summary>
+    protected override void EnterPostMinigame()
+    {
+        base.EnterPostMinigame();
+
+        Interact();
+    }
+
+    /// <summary>
+    /// Runs a timer that when complete will set the Robot to its failure state
+    /// </summary>
+    /// <returns>Waits one second</returns>
+    private IEnumerator DeathTimer()
+    {
+        while (_timeElapsed < _timeToDeath)
+        {
+            yield return new WaitForSeconds(1f);
+
+            _timeElapsed += 1f;
+        }
+
+        if (_currentState == NpcStates.DefaultIdle)
+        {
+            EnterFailure();
+        }
+    }
+}
