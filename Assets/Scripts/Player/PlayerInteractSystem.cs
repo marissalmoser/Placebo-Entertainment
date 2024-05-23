32fbf755aa21d2ad87be0df6ef066f1657c725ad
AlecatEnjoyer <Mark25608@yahoo.com>
2024-05-23 15:44:25 -0500

    player interact system part 1

---
+++ Assets/Scripts/Player/PlayerInteractSystem.cs
@@ -0,0 +1,56 @@
+/*****************************************************************************
+// File Name :         PlayerInteractSystem.cs
+// Author :            Mark Hanson
+// Creation Date :     5/22/2024
+//
+// Brief Description : Class used for detecting interactable objects 
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractSystem
+{
+    public string ObjectName;
+
+    /// <summary>
+    /// When adding no variable within a new player interaction goes to the default which is none
+    /// </summary>
+    public PlayerInteractSystem()
+    {
+        ObjectName = "Default None";
+    }
+    /// <summary>
+    /// A function used to switch out the object name used for when its actually detecting a new object
+    /// </summary>
+    /// <param name="_newDetection"></param>
+    public PlayerInteractSystem(string _newDetection)
+    {
+        ObjectName = _newDetection;
+    }
+    /// <summary>
+    /// A function to switch back to default detection which is none
+    /// </summary>
+    public void BackToDefault()
+    {
+        ObjectName = "Default None";
+    }
+    /// <summary>
+    /// Whenever pressing E it runs through the list of different interactable objects and which ones zone its in 
+    /// </summary>
+    public void CallInteract()
+    {
+        switch (ObjectName)
+        {
+            case "Test":
+                    Debug.Log("E has been pressed");
+                break;
+            case "Default None":
+                Debug.Log("No action was done");
+                break;
+            default:
+                break;
+            
+        }
+    }
+}
