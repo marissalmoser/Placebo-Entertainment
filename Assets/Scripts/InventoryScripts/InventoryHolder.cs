d45c60b78685a4161664bff03f105b9c7d3a39fe
evroman03 <evroman@mail.bradley.edu>
2024-05-28 21:08:08 -0500

    Ihategithub

--- Assets/Scripts/InventoryScripts/InventoryHolder.cs
+++ Assets/Scripts/InventoryScripts/InventoryHolder.cs
@@ -16,56 +16,10 @@ public class InventoryHolder : MonoBehaviour
 
     [SerializeField] protected InventorySystem inventorySystem;
     [SerializeField] private int inventorySize;
-
-    public InventoryItemData data;
     public InventorySystem InventorySystem => inventorySystem;
-    public int temp;
     private void Awake()
     {
         inventorySystem = new InventorySystem(inventorySize);
         
-    }
-    public void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.Alpha1))
-        {
-            inventorySystem.AddToInventory(data, 1, out temp);
-            print(temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha2))
-        {
-            inventorySystem.AddToInventory(data, 2, out temp);
-            print(temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha3))
-        {
-            inventorySystem.AddToInventory(data, 3, out temp);
-            print(temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha4))
-        {
-            inventorySystem.AddToInventory(data, 4, out temp);
-            print(temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha5))
-        {
-            inventorySystem.RemoveFromInventory(data, 1, true, out _, out temp);
-            print("Removed: " + temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha6))
-        {
-            inventorySystem.RemoveFromInventory(data, 2, true, out _, out temp);
-            print("Removed: " + temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha7))
-        {
-            inventorySystem.RemoveFromInventory(data, 3, true, out _, out temp);
-            print("Removed: " + temp);
-        }
-        if (Input.GetKeyUp(KeyCode.Alpha8))
-        {
-            inventorySystem.RemoveFromInventory(data, 4, true, out _, out temp);
-            print("Removed: " + temp);
-        }
     }
 }
