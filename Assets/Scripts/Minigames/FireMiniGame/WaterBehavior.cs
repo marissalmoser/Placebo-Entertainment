399e2c158a296790eacb039c56a64c7cd22f027f
AlecatEnjoyer <Mark25608@yahoo.com>
2024-06-21 10:44:26 -0500

    Fire/fish minigame

---
+++ Assets/Scripts/Minigames/FireMiniGame/WaterBehavior.cs
@@ -0,0 +1,31 @@
+/*****************************************************************************
+// File Name :         WaterBehavior.cs
+// Author :            Mark Hanson
+// Creation Date :     6/19/2024
+//
+// Brief Description : Function for destroying water when its in contact with specific things.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBehavior : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private GameObject _cam;
+
+    void Awake()
+    {
+        _cam = GameObject.FindWithTag("MainCamera");
+        _rb.AddForce(_cam.transform.forward * 9f, ForceMode.Impulse);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name != "FishHose" && col.gameObject.tag != "Water")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
