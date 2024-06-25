7f73b9eb9381e7df5b8f1d3e8c5117a3c6e25ed7
evroman03 <evroman@mail.bradley.edu>
2024-06-24 22:00:47 -0500

    ScriptsMade

---
+++ Assets/Scripts/NpcBehaviors/FishNpc.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishNpc : BaseNpc
+{
+    [SerializeField] private InventoryItemData d;
+    private bool _enteredFireRoom = false;
+    [SerializeField] private int secondsUntilFailFireGame;
+
+    public override void CheckForStateChange()
+    {
+        if(_currentState == NpcStates.DefaultIdle && _enteredFireRoom)
+        {
+            EnterMinigameReady();
+        }
+        else if (_currentState == NpcStates.PlayingMinigame)
+        {
+            EnterPostMinigame();
+        }
+    }
+    protected override void EnterMinigameReady()
+    {
+        base.EnterMinigameReady(); 
+    }
+    protected override void EnterPostMinigame()
+    {
+        base.EnterPostMinigame();  
+    }
+    protected override void EnterIdle()
+    {
+        base.EnterIdle();
+        TimerManager.Instance.CreateTimer("FireRoomMiniGameTimer", secondsUntilFailFireGame);
+    }
+}
