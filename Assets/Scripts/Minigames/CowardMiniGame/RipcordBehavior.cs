5aa9d471c7c24d3111fd282c5977d278103fc398
AlecatEnjoyer <Mark25608@yahoo.com>
2024-07-17 10:47:13 -0500

    lights working

--- Assets/Scripts/Minigames/CowardMiniGame/RipcordBehavior.cs
+++ Assets/Scripts/Minigames/CowardMiniGame/RipcordBehavior.cs
@@ -21,7 +21,7 @@ public class RipcordBehavior : MonoBehaviour
 
     [Header("Points of Movement")]
     [SerializeField] private float _maxReach;
-    [SerializeField]private GameObject _targetFollow;
+    [SerializeField] private GameObject _targetFollow;
     [SerializeField] private float _speed;
     [SerializeField] bool PressedE;
     private Vector3 _relocatePoint;
@@ -33,6 +33,16 @@ public class RipcordBehavior : MonoBehaviour
     private bool _doOnce;
     private int _numReleased;
     private GameObject _preview;
+
+
+
+    private bool _gameStarted;
+
+    [Header("Light")]
+    private int _currentLight;
+    [SerializeField] private GameObject[] _listLights;
+    private float _lightSpeed;
+    private bool _lightDoOnce;
 
     [SerializeField] private GameObject _gears;
 
@@ -47,15 +57,17 @@ public class RipcordBehavior : MonoBehaviour
         _hasReleased = false;
         _numReleased = 0;
         _relocatePoint = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-
+        _gameStarted = false;
         GameObject _pcObject = GameObject.FindWithTag("Player");
         _pc = _pcObject.GetComponent<PlayerController>();
+        _currentLight = 0;
+        _lightDoOnce = true;
     }
     // Update is called once per frame
     void Update()
     {
         _successfulPulls.text = _numReleased.ToString();
-        if (_pc.Interact.IsPressed() && PressedE == true && transform.position.z > _targetFollow.transform.position.z + 1.7f && _numReleased != 3)
+        if (_pc.Interact.IsPressed() && PressedE == true && transform.position.z > _targetFollow.transform.position.z + 1.7f && _numReleased != 3 && _gameStarted == true)
         {
             transform.position -= new Vector3(0, 0, _speed * Time.deltaTime);
             StartCoroutine(FindPreview());
@@ -64,9 +76,9 @@ public class RipcordBehavior : MonoBehaviour
                 _preview.GetComponent<Collider>().enabled = false;
             }
         }
-        if(_pc.Interact.IsPressed() && PressedE == true && _doOnce2 == true && _numReleased != 3)
+        if (_pc.Interact.IsPressed() && PressedE == true && _doOnce2 == true && _numReleased != 3 && _gameStarted == true)
         {
-            Vector3 _previewerPoint = new Vector3(_relocatePoint.x, _relocatePoint.y, Random.Range(_maxReach,_relocatePoint.z));
+            Vector3 _previewerPoint = new Vector3(_relocatePoint.x, _relocatePoint.y, Random.Range(_maxReach, _relocatePoint.z));
             Instantiate(_ReleasePreviewer, _previewerPoint, Quaternion.identity);
             _doOnce2 = false;
         }
@@ -89,7 +101,7 @@ public class RipcordBehavior : MonoBehaviour
                 GameObject.Destroy(_preview);
             }
         }
-        if(PressedE == false && _doOnce == true)
+        if (PressedE == false && _doOnce == true)
         {
             StartCoroutine(FindPreview());
             if (_preview != null && _preview.transform.position.z > transform.position.z)
@@ -98,13 +110,43 @@ public class RipcordBehavior : MonoBehaviour
                 _doOnce = false;
             }
         }
-        if(_numReleased == 3)
+        if (_numReleased == 0 && _gameStarted == true && _lightDoOnce == true)
         {
+            _currentLight = 0;
+            StartCoroutine(blinkingLight());
+            _lightDoOnce = false;
+        }
+        if (_numReleased == 1 && _gameStarted == true && _lightDoOnce == true)
+        {
+            _listLights[0].SetActive(true);
+            _currentLight = 1;
+            StartCoroutine(blinkingLight());
+            _lightDoOnce = false;
+        }
+        if(_numReleased == 2 && _gameStarted == true && _lightDoOnce == true)
+        {
+            _listLights[1].SetActive(true);
+            _currentLight = 2;
+            StartCoroutine(blinkingLight());
+            _lightDoOnce = false;
+        }
+        if (_numReleased == 3)
+        {
+            _listLights[2].SetActive(true);
+            StopCoroutine(blinkingLight());
             _gears.SetActive(true);
             _successfulPulls.color = Color.green;
             //Destroy(this);
         }
         //Debug.Log(_numReleased);
+
+    }
+    IEnumerator blinkingLight()
+    {
+        _listLights[_currentLight].SetActive(true);
+        yield return new WaitForSeconds(_lightSpeed);
+        _listLights[_currentLight].SetActive(false);
+        _lightDoOnce = true;
     }
     IEnumerator ReleaseWindow()
     {
@@ -119,13 +161,13 @@ public class RipcordBehavior : MonoBehaviour
     }
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Release" && _hasReleased == true)
+        if (col.gameObject.tag == "Release" && _hasReleased == true)
         {
             _numReleased++;
             GameObject.Destroy(col.gameObject);
         }
 
-        if(col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player")
         {
             PressedE = true;
             TabbedMenu.Instance.ToggleInteractPrompt(true, "RIPCORD");
@@ -139,5 +181,9 @@ public class RipcordBehavior : MonoBehaviour
             PressedE = false;
             TabbedMenu.Instance.ToggleInteractPrompt(false);
         }
+    }
+    public void GameStart()
+    {
+        _gameStarted = true;
     }
 }
