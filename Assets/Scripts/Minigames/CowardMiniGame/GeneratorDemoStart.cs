d74a41cfee2180eae95628d9125aa11b77985e8c
ASLH <andreaswihartdecoster@gmail.com>
2024-07-20 23:05:05 -0500

    Generator Test Gym

---
+++ Assets/Scripts/Minigames/CowardMiniGame/GeneratorDemoStart.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorDemoStart : MonoBehaviour
+{
+    [SerializeField] private GameObject _manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _manager.GetComponent<RipcordBehavior>().GameStart();
+    }
+}
